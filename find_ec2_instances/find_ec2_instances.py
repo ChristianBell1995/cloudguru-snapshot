151eba469c0edb4f25a78c06b86b5e91547aa464
christian bell <christiantcbell@gmail.com>
2019-12-30 18:17:57 +0000

    added option to view just the latest snapshot or all of them for a volume

--- find_ec2_instances/find_ec2_instances.py
+++ find_ec2_instances/find_ec2_instances.py
@@ -44,7 +44,8 @@ def snapshots():
 
 @snapshots.command('list')
 @click.option('--project', default=None, help="Only volumes for project(tag Project:<name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True, help="List all snapshots for a volume not just the most recent one")
+def list_snapshots(project, list_all):
     "List volume snapshots"
     instances = filter_instances(project)
 
@@ -59,6 +60,8 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime('%c')
                 )))
+
+                if s.state == 'completed' and not list_all: break
     return
 
 
